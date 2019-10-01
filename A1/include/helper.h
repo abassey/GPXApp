01b8c4d9961ec57c6a845892cb19673bb59ffaeb
Anemmeabasi Bassey <anemmeabasi1@gmail.com>
2019-10-01 19:59:05 -0400

    new version

--- A1/include/helper.h
+++ A1/include/helper.h
@@ -1,3 +1,6 @@
+#ifdef HELPER_H
+#define HELPER_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,4 +11,11 @@
 GPXdoc * new_gpxdoc(void);
 Waypoint * new_waypoint(void);
 Track * new_track(void);
-Route * new_route (void);
+TrackSegment * new_tracksegment(void);
+Route * new_route(void);
+char * new_string(void);
+Attribute * new_attribute(void);
+void free_string(char * toFree);
+void create_doc_from_tree(xmlDoc * doc, GPX ** doc);
+
+#endif
