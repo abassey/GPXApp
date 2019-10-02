0862f3dd8930a919e7b1eddd4f7bf8a9d4dbb95d
Anemmeabasi Bassey <anemmeabasi1@gmail.com>
2019-10-01 20:00:12 -0400

    new version

--- A1/src/GPXParser.c
+++ A1/src/GPXParser.c
@@ -1,23 +1,29 @@
 #include "GPXParser.h"
 #include "helper.h"
+#include "LinkedListAPI.h"
 
 GPXdoc* createGPXdoc(char* fileName)
 {
-    if fileName == NULL return NULL;
+    if(fileName == NULL) return NULL;
 
     xmlDoc * doc = NULL;
-    xmlNode * root_element = NULL;
 
     LIBXML_TEST_VERSION
 
     doc = xmlReadFile(argv[1], NULL, 0);
     root_element = xmlDocGetRootElement(doc);
 
-    if doc == NULL return NULL;
+    if (doc == NULL && root_element == NULL) return NULL;
+    
+   // char* (*wpToStr)(void*) = &waypointToString;
+	//void (*delWp)(void*) = &free_string;
+	//int (*compWp)(void*) = &compare_strings;
+    
+    //List * tree = initializeList();
 
     GPXdoc * gdoc = new_gpxdoc();
     
-    
+    //turn tree doc to list, then iterate through the list
     
     xmlFreeDoc(doc);
     xmlCleanupParser();
@@ -26,13 +32,34 @@ GPXdoc* createGPXdoc(char* fileName)
 
 char* GPXdocToString(GPXdoc* doc)
 {
-    if doc == NULL return NULL;
+    if(doc == NULL) return NULL;
 
-
+	char * temp = new_string();
+	char buffer[50];
+	
+	strcpy(temp, "namespace: ");
+	strcpy(temp, doc->namespace);
+	strcpy(temp, ", version: ");
+	gcvt(doc->version, 10, buffer);
+	strcpy(temp, buffer);
+	strcpy(temp, ", creator: ");
+	strcpy(temp, doc->creator);
+	strpy(temp, ", waypoints: [");
+	strcpy(temp, toString(doc->waypoints));
+	strcpy(temp, "], routes: [");
+	strcpy(temp, toString(doc->routes));
+	strcpy(temp, "], tracks: [");
+	strcpy(temp, toString(doc->tracks));
+	strcpy(temp, "]");
+	
+	return temp;
+	
 }
 
 void deleteGPXdoc(GPXdoc* doc)
 {
+	if(doc == NULL) return NULL;
+	
     free(doc->creator);
 
     //free each waypoint with list iterator then free whole list
@@ -48,6 +75,8 @@ void deleteGPXdoc(GPXdoc* doc)
 
 int getNumWaypoints(const GPXdoc* doc)
 {
+	if(doc == NULL) return NULL;
+	
     int num_waypoints = 0;
 
     num_waypoints = getLength(doc->waypoints);
@@ -57,6 +86,8 @@ int getNumWaypoints(const GPXdoc* doc)
 
 int getNumRoutes(const GPXdoc* doc)
 {
+	if(doc == NULL) return NULL;
+	
     int num_routes = 0;
 
     num_routes = getLength(doc->routes);
@@ -66,6 +97,8 @@ int getNumRoutes(const GPXdoc* doc)
 
 int getNumTracks(const GPXdoc* doc)
 {
+	if(doc == NULL) return NULL;
+	
     int num_tracks = 0;
 
     num_tracks = getLength(doc->tracks);
@@ -75,6 +108,8 @@ int getNumTracks(const GPXdoc* doc)
 
 int getNumSegments(const GPXdoc* doc)
 {
+	if(doc == NULL) return NULL; 
+	
     int num_segments = 0;
 
     num_segments = getLength(doc->waypoints->segments);
@@ -84,32 +119,93 @@ int getNumSegments(const GPXdoc* doc)
 
 int getNumAttributes(const GPXdoc* doc)
 {
-
+	if(doc == NULL) return NULL; 
+	
+	int num_attributes = 0;
+	
+	num_attributes = getLength(doc->tracks->attributes);
+	num_attributes += getLength(doc->routes->attirbutes);
+	num_attributes += getLength(doc->waypoints->attirbutes);
+	
+	return num_attributes;
 }
 
 Waypoint* getWaypoint(const GPXdoc* doc, char* name)
 {
+	if(doc == NULL || name == NULL) return NULL;
+	
     Waypoint * found_waypoint = new_waypoint();
+	strcpy(found_waypoint->name, name);
+	
+    ListIterator itr = createIterator(doc->tracks);
 
-    findElement(doc->waypoints, compareWaypoints(), name);
-    //find out what this returns/how this works
+	void* data = nextElement(&itr);
+	while (data != NULL)
+	{
+		if (compareWaypoints(data, found_track))
+			{
+				found_waypoint->longitude = data->longitude;
+				found_waypoint->latitude = data->latitude;
+				found_waypoint->attributes = data->attributes;
+				return found_waypoint;
+			}
+
+		data = nextElement(&itr);
+	}
+
+	return NULL;
 }
 
 Track* getTrack(const GPXdoc* doc, char* name)
 {
-     Track * found_track = new_track();
+	if(doc == NULL || name == NULL) return NULL;
+	
+    Track * found_track = new_track();
+    strcpy(found_track->name, name);
 
-    findElement(doc->tracks, compareTracks(), name);
-    //find out what this returns/how this works
+    ListIterator itr = createIterator(doc->tracks);
+
+	void* data = nextElement(&itr);
+	while (data != NULL)
+	{
+		if (compareTracks(data, found_track))
+			{
+				found_track->segments = data->segments;
+				found_track->attributes = data->attributes;
+				return found_track;
+			}
+
+		data = nextElement(&itr);
+	}
+
+	return NULL;
 }
 
 Route* getRoute(const GPXdoc* doc, char* name)
 {
+	 if(doc == NULL) return NULL;
+	 
      Route * found_route = new_route();
+     strcpy(found_route->name, name);
+     
+     ListIterator itr = createIterator(doc->routes);
 
-    findElement(doc->routes, compareRoutes(), name);
-    //find out what this returns/how this works
+	void* data = nextElement(&itr);
+	while (data != NULL)
+	{
+		if (compareRoutes(data, found_track))
+			{
+				found_route->waypoints = data->waypoints;
+				found_route->attributes = data->attributes;
+				return found_route;
+			}
+
+		data = nextElement(&itr);
+	}
+
+	return NULL;
 }
+
 
 /* ******************************* List helper functions  - MUST be implemented *************************** */
 
@@ -123,13 +219,24 @@ void deleteAttribute( void* data)
 
 char* attributeToString( void* data)
 {
-    char * temp = (Attribute *) data;
+	Attribute * att = (Attribute *) data;
+    char * temp = new_string();
+    strcpy(temp, "name: ");
+    strcpy(temp, att->name);
+    strcpy(temp, ", ");
+    strcpy(temp, "value: ");
+    strcpy(temp, att->value);
+    
+    return temp;
 
 }
 
 int compareAttributes(const void *first, const void *second)
 {
-
+	const Attribute * a_first = (const Attribute *) first;
+	const Attribute * b_second = (const Attribute *) second;
+	
+	return (strcmp(a_first->name, b_second->name) && strcmp(a_first->value, b_second->value));
 }
 
 void deleteWaypoint(void* data)
@@ -144,28 +251,33 @@ char* waypointToString( void* data)
 {
     char buffer[50];
 
-    char * temp = (Waypoint *) data;
+    Waypoint * wp = (Waypoint *) data;
+    char * temp = new_string();
     strcpy(temp,"name: ");
-    strcpy(temp,data->name);
+    strcpy(temp,wp->name);
     strcpy(temp,", ");
 
     strcpy(temp,"longitude: ");
-    gcvt(data->longitude, 10, buffer);
+    gcvt(wp->longitude, 10, buffer);
     strcpy(temp, buffer);
     strcpy(temp,", ");
 
     strcpy(temp,"latitude: ");
-    gcvt(data->latitude, 10, buffer);
+    gcvt(wp->latitude, 10, buffer);
     strcpy(temp,buffer);
-    strcpy(temp,", ");
+    strcpy(temp,", [");
 
-    strcpy(temp,toString(data->attributes));
+    strcpy(temp,toString(wp->attributes));
+    strcpy(temp, "]");
     return temp;
 }
 
 int compareWaypoints(const void *first, const void *second)
 {
-
+	const Waypoint * a_first = (const Waypoint *) first;
+	const Waypoint * b_second = (const Waypoint *) second;
+	
+	return strcmp(a_first->name, b_second->name);
 }
 
 void deleteRoute(void* data)
@@ -179,40 +291,82 @@ void deleteRoute(void* data)
 
 char* routeToString(void* data)
 {
-
+	Route * rt = (Route *) data;
+	
+	char * temp = new_string();
+	strcpy(temp, "name: ");
+	strcpy(temp, rt->name);
+	strcpy(temp, ", [");
+	strcpy(temp, toString(rt->waypoints));
+	strcpy(temp, "], [");
+	strcpy(temp, toString(rt->attributes));
+	strcpy(temp, " ]");
+	
+	return temp;
 }
 
 int compareRoutes(const void *first, const void *second)
 {
-
+	const Route * a_first = (const Route *) first;
+	const Route * b_second = (const Route *) second;
+	
+	return (strcmp(a_first->name, b_second->name));
 }
 
 void deleteTrackSegment(void* data)
 {
-
+	TrackSegment * temp = (TrackSegment *) data;
+	freeList(temp->waypoints);
+	free(temp);
 }
 
 char* trackSegmentToString(void* data)
 {
-
+	TrackSegment * ts = (TrackSegment *) data;
+	char * temp = new_string();
+	
+	strcpy(temp, toString(ts->waypoints));
+	
+	return temp;
 }
 
 int compareTrackSegments(const void *first, const void *second)
 {
-
+	if(first == second) return 0;
+	else if (first < second) return -1;
+	 
+	return 1;
 }
 
 void deleteTrack(void* data)
-{
-
+{	
+	Track * temp = (Track *) data;
+	free(temp->name);
+	freeList(temp->segments);
+	freeList(temp->attributes);
+	free(temp);
 }
 
 char* trackToString(void* data)
 {
-
+	Track * tr = (Track *) data;
+	char * temp = new_string();
+	
+	strcpy(temp, "name: ");
+	strcpy(temp, tr->name);
+	strcpy(temp, ", [");
+	strcpy(temp, toString(tr->segments));
+	strcpy(temp, "] , [");
+	strcpy(temp, toString(tr->attributes));
+	strcpy(temp, "] ");
+	
+	return temp;
 }
 
 int compareTracks(const void *first, const void *second)
 {
-
+	const Track * a_first = (const Track *) first;
+	const Track * b_second = (const Track *) second;
+	
+	return (strcmp(a_first->name, b_second->name));
 }
--- A1/src/helper.c
+++ A1/src/helper.c
@@ -2,5 +2,184 @@
 
 GPXdoc * new_gpxdoc()
 {
+    GPXdoc * gdoc = malloc(sizeof(GPXdoc));
+    gdoc->version = 1.1;
+    gdoc->creator = new_string();
     
+    char* (*rteToStr)(void*) = &routeToString;
+	void (*delRte)(void*) = &deleteRoute;
+	int (*compRte)(void*) = &compareRoutes;
+    
+    gdoc->routes = initializeList(rteToStr, delRte, compRte);
+    
+    char* (*wpToStr)(void*) = &waypointToString;
+	void (*delWp)(void*) = &deleteWaypoint;
+	int (*compWp)(void*) = &compareWaypoints;
+    
+    gdoc->waypoints = initializeList(wpToStr, delWp, compWp);
+    
+    char* (*trToStr)(void*) = &trackToString;
+	void (*delTr)(void*) = &deleteTrack;
+	int (*compTr)(void*) = &compareTracks;
+    
+    gdoc->tracks = initializeList(trToStr, delTr, compTr);
+    
+    return gdoc;
 }
+
+Attribute * new_attribute()
+{
+	Attribute * att = malloc(sizeof(Attribute));
+	att->name = new_string();
+	att->value = new_string();
+	
+	return att;
+}
+
+Waypoint * new_waypoint(void)
+{
+	Waypoint * wp = malloc(sizeof(Waypoint));
+	wp->name = new_string();
+	wp->longitude = 0.0;
+	wp->latitude = 0.0;
+	
+	char* (*attrToStr)(void*) = &attributeToString; //fuction pointer. make for each one
+	void (*delAttr)(void*) = &deleteAttribute;
+	int (*compAttr)(void*) = &compareAttributes;
+	
+	wp->attributes = initializeList(attToStr, delAttr, compAttr);
+	
+	return wp;
+}
+
+Route * new_route(void)
+{
+	Route * rte = malloc(sizeof(Route));
+	rte->name = new_string();
+	
+	char* (*wpToStr)(void*) = &waypointToString;
+	void (*delWp)(void*) = &deleteWaypoint;
+	int (*compWp)(void*) = &compareWaypoints;
+	
+	rte->waypoints = initializeList(wpToStr, delWp, compWp);
+	
+	char* (*attrToStr)(void*) = &attributeToString;
+	void (*delAttr)(void*) = &deleteAttribute;
+	int (*compAttr)(void*) = &compareAttributes;
+	
+	rte->attributes = initializeList(attrToStr, delAttr, compAttr);
+	
+	return rte;
+}
+
+TrackSegment * new_tracksegment(void)
+{
+	TrackSegment * ts = malloc(sizeof(TrackSegment));
+	
+	char* (*attrToStr)(void*) = &attributeToString;
+	void (*delAttr)(void*) = &deleteAttribute;
+	int (*compAttr)(void*) = &compareAttributes;
+	
+	ts->attributes = initializeList(attrToStr, delAttr, compAttr);
+	
+	return ts;
+}
+
+Track * new_track(void)
+{
+	Track * tr = malloc(sizeof(Track));
+	tr->name = new_string();
+	
+	char* (*tsToStr)(void*) = &trackSegmentToString;
+	void (*delTs)(void*) = &deleteTrackSegment;
+	int (*compTs)(void*) = &compareTrackSegments;
+	
+	tr->segments = initializeList(tsToStr, delTs,compTs);
+	
+	char* (*attrToStr)(void*) = &attributeToString;
+	void (*delAttr)(void*) = &deleteAttribute;
+	int (*compAttr)(void*) = &compareAttributes;
+	
+	tr->attributes = initializeList(attrToStr, delAttr, compAttr);
+	
+	return tr;
+}
+
+char * new_string(void)
+{
+	char * string = calloc(50, sizeof(char));
+	
+	return string;
+}
+
+void free_string(char * toFree)
+{
+	free(toFree);
+}
+
+int compare_strings(const void * first, const void * second)
+{
+	const char * a_first = (const char *) first;
+	const char * b_second = (const char *) second;
+	
+	return strcmp(a_first, b_second);
+}
+
+void create_doc_from_tree(xmlDoc * doc, GPX ** gdoc)
+{
+	xmlNode * root = NULL;
+	root = xmlDocGetRootElement(doc);
+	
+    mlNode *cur_node = NULL;
+
+    for (cur_node = root; cur_node != NULL; cur_node = cur_node->next) {
+    {
+        if (cur_node->type == XML_ELEMENT_NODE) {
+            printf("node type: Element, name: %s\n", cur_node->name);
+            
+            if(strcmp(cur_node->name, "gpx") == 0) 
+            {
+				//go to its attributes
+				//assign version, creator, etc
+			}
+			
+			if(strcmp(cur_node->name, "route") == 0) 
+            {
+				//go to its attributes
+				//assign all that needs to be
+			}
+			
+			if(strcmp(cur_node->name, "waypoint") == 0) 
+            {
+				//go to its attributes
+				//assign all that needs to be
+			}
+			
+			if(strcmp(cur_node->name, "track") == 0) 
+            {
+				//go to its attributes
+				//assign all that needs to be
+			}
+        }
+	}
+
+         if (cur_node->content != NULL ){
+             printf("  content: %s\n", cur_node->content);
+         }
+
+        xmlAttr *attr;
+        
+        for (attr = cur_node->properties; attr != NULL; attr = attr->next)
+        {
+            xmlNode *value = attr->children;
+            char *attrName = (char *)attr->name;
+            char *cont = (char *)(value->content);
+            printf("\tattribute name: %s, attribute value = %s\n", attrName, cont);
+        }
+
+        create_doc_from_tree(cur_node->children, gdoc);
+    }
+}
+
+
+
